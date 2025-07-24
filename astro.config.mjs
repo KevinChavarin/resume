ad89cc5569d61085d9256708b13d9910d15dc9d0
Kevin Chavarin Pineda <Kevin.chavarin@alumnos.udg.mx>
2025-07-24 10:38:09 -0600

    Adding server validations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,16 @@ import react from '@astrojs/react';
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
+      preview: {
+        host: true,
+        port: Number(process.env.PORT) || 4321,
+        allowedHosts: ['all']
+      }
   },
   integrations: [react()],
   output: 'static',
   server: {
-    host: '0.0.0.0', // Permite conexiones externas
+    host: '0.0.0.0',
     port: Number(process.env.PORT) || 4321,
   },
 });
